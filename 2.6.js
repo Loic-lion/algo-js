fce4c462cfb6865f257690e7be67a0fca0b91633
Loic-lion <loic.lion.contact@gmail.com>
2023-04-06 15:26:57 +0200

    Add files via upload
    ajout de l'exo 2.6

---
+++ 2.6.js
@@ -0,0 +1,24 @@
+let x = new Number(prompt("rentrez un chiffre entre 1 et 7")).valueOf();
+switch (x) {
+  case 1:
+    alert("lundi");
+    break;
+  case 2:
+    alert("mardi");
+    break;
+  case 3:
+    alert("mercredi");
+    break;
+  case 4:
+    alert("jeudi");
+    break;
+  case 5:
+    alert("vendredi");
+    break;
+  case 6:
+    alert("samedi");
+    break;
+  case 7:
+    alert("dimanche");
+    break;
+}
