34b7a5bc81cc49640df9d93eba01c1e871279156
Loic-lion <loic.lion.contact@gmail.com>
2023-04-06 10:32:43 +0200

    Add files via upload
    ajout du premier exercice.

---
+++ 1.0.js
@@ -0,0 +1,12 @@
+let number1 = 30;
+let number2 = 10;
+//addition//
+console.log(number1 + number2);
+//multiplication//
+console.log(number2 * number1);
+//soustraction//
+console.log(number1 - number2);
+//division//
+console.log(number2 / number1);
+//modulo//
+console.log(number1 % number2);
