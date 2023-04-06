5111a42a7d83b464fe96b25b1f2b6a755c75e1cf
Loic-lion <loic.lion.contact@gmail.com>
2023-04-06 16:21:18 +0200

    Add files via upload
    ajout de l'exo 3.0

---
+++ 3.0.js
@@ -0,0 +1,25 @@
+let arr = [
+  "Abel",
+  "Alexendra",
+  "Anthony",
+  "Benjamin",
+  "Bruno",
+  "Delphine",
+  "Emilien",
+  "Ethan",
+  "Ismael",
+  "Jodie",
+  "Julien",
+  "Louka",
+  "Loïc",
+  "Luciano",
+  "Marine",
+  "Nikko",
+  "Noa",
+  "Steve",
+  "Sylvain",
+  "Willy",
+];
+for (let elem of arr) {
+  alert(elem);
+}
