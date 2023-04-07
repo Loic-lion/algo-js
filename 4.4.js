800031d5805ec0812c17c6cc20445fe2570cef58
Loic Lion <loic.lion.contact@gmail.com>
2023-04-07 15:22:12 +0200

    ajout 4.4

--- 4.4.js
+++ 4.4.js
@@ -1,1 +1,32 @@
-function pickLearner(inputAr, n)
+let arr = [
+  "Abel",
+  "Alexendra",
+  "Anthony",
+  "Benjamin",
+  "Bruno",
+  "Delphine",
+  "Emilien",
+  "Ethan",
+  "Ismael",
+  "Jodie",
+  "Julien",
+  "Louka",
+  "Loïc",
+  "Luciano",
+  "Marine",
+  "Nikko",
+  "Noa",
+  "Steve",
+  "Sylvain",
+  "Willy",
+];
+
+function pickLearner(inputAr, n) {
+  let randomar = [];
+  for (let i = 0; i < n; i++) {
+    let randomlearner = inputAr[Math.floor(Math.random() * inputAr.length)];
+    randomar.push(randomlearner);
+  }
+  return randomar;
+}
+alert(pickLearner(arr, 4));
